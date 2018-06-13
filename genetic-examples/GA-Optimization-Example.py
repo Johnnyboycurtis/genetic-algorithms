681e4a79d5e160f7fe37ec5b094a09aa8c8b1a92
JN107154 <jn107154@dell.corp.int>
2018-06-13 15:42:19 -0700

    GA TSP solution script for Udemy course

--- genetic-examples/GA-Optimization-Example.py
+++ genetic-examples/GA-Optimization-Example.py
@@ -7,6 +7,8 @@ Created on Fri May  4 09:01:44 2018
 """
 
 '''
+This script is based on ga-example-3.py
+
 GENETIC ALGORITHM STEPS
 
 1. CREATE INITIAL POPULATION
@@ -32,15 +34,6 @@ def _fitness(x):
     else:
         return 0
 
-'''
-
-def _fitness(x):
-    if x > -11 and x < 11:
-        y = x**2 + x + 10
-        return round(y, 6)
-    else:
-        return 0
-'''
 
 fitness = np.vectorize(_fitness)
 
@@ -54,30 +47,30 @@ def _get_fittest_parent(parents, fitness):
     return round(best_parent, 4), round(best_fitness, 4)
     
 
-def _mutate(parents, fitness_function, decay = 0.01):
-    n = int(len(parents) * (1-decay))
+def _mutate(parents, fitness_function):
+    n = int(len(parents))
     scores = fitness_function(parents)
     idx = scores > 0 ## positive values only
     scores = scores[idx]
     parents = np.array(parents)[idx]
     ## resample parents with probabilities proportional to fitness
     ## then, add some noise for 'random' mutation
-    children = np.random.choice(parents, size=n, p = scores / scores.sum()) + np.random.uniform(-0.51,0.51, size=n)
-    return children.tolist() ## new parents
+    children = np.random.choice(parents, size=n, p = scores / scores.sum()) 
+    children = children + np.random.uniform(-0.51,0.51, size=n)  # add some noise to mutate
+    return children.tolist() ## convert array to list
+    
     
 
 def find_best_fit(parents, fitness_function, mutate_fn, popsize = 100, max_iter = 100):
     History = []
     ## initial parents; gen zero
     best_parent, best_fitness = _get_fittest_parent(parents, fitness)
-    
-    x = np.linspace(start=-20, stop=20, num=100)
-    
+        
+    # first plot the initial parents
+    x = np.linspace(start=-20, stop=20, num=200) ## population range
     plt.plot(x, fitness_function(x))
-    plt.scatter(parents, fitness_function(parents), marker= 'x')    
+    plt.scatter(parents, fitness_function(parents), marker= 'x')
     
-    ## first generation
-    _x = [-np.inf]
     ## next generation
     for i in range(1, max_iter + 1):
         parents = _mutate(parents, fitness_function=fitness_function)
@@ -92,7 +85,6 @@ def find_best_fit(parents, fitness_function, mutate_fn, popsize = 100, max_iter 
             
         curr_parent, curr_fitness = _get_fittest_parent(parents, fitness_function)
         print('generation {}| best fitness {}| current fitness {} | current_parent {}'.format(i, best_fitness, curr_fitness, curr_parent))
-        _x.append(np.max(fitness_function(parents)))
         History.append((i, np.max(fitness_function(parents))))
         
         ax.remove() ## remove points
@@ -100,7 +92,7 @@ def find_best_fit(parents, fitness_function, mutate_fn, popsize = 100, max_iter 
     
     print(best_parent, best_fitness)
     plt.scatter(parents, fitness_function(parents))
-    plt.scatter(best_parent, fitness_function(best_parent), marker = '.', c = 'b', s = 200)
+    plt.scatter(best_parent, fitness_function(best_parent), marker = '.', c = 'b', s = 100)
     plt.pause(0.09)
     plt.ioff()
     ## return best parents
